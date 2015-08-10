8bc2c424133d993cb9446670705a2cf1eaf72d1e
Ed Santilli <santilli@physics.unc.edu>
2015-08-10 08:10:42 -0400

    Changed module and function names to fit fortran 90 conventions.

--- src/MGPoisson2D.f90
+++ src/MGPoisson2D.f90
@@ -26,95 +26,87 @@
 ! ------------------------------------------------------------------------------
 ! Defines the precision of all numbers that we will use.
 ! ------------------------------------------------------------------------------
-module Precision
+module precision
     integer, parameter:: dp = kind(0.d0)
-end module Precision
+end module precision
 
 
 ! ------------------------------------------------------------------------------
 ! ------------------------------------------------------------------------------
-module ArrayUtils
-    use Precision
+module arrayutils
+    use precision
     implicit none
 
     ! --------------------------------------------------------------------------
     ! Array index extents and size info.
     ! --------------------------------------------------------------------------
-    type Box
+    type box
         integer  :: ilo, ihi        ! Lower and upper array indices in x dir
         integer  :: jlo, jhi        ! Lower and upper array indices in y dir
         integer  :: nx, ny          ! Total number of cells/nodes in each dir
         real(dp) :: dx, dy          ! The grid spacing
-    end type Box
-
-
-    ! --------------------------------------------------------------------------
-    ! Used to identify the type of BC at each boundary.
-    ! --------------------------------------------------------------------------
-    type BCTypes
-        integer :: xlo, xhi
-        integer :: ylo, yhi
-    end type BCTypes
+    end type box
 
 
     ! --------------------------------------------------------------------------
     ! These are the valid types of BCs.
     ! --------------------------------------------------------------------------
-    integer, parameter :: UNDEFINED = -2
-    integer, parameter :: NONE = -1
-    integer, parameter :: NEUM = 0
-    integer, parameter :: DIRI = 1
-    integer, parameter :: PERIODIC = 2
-    integer, parameter :: CF = 3
+    integer, parameter :: BC_UNDEFINED = -2
+    integer, parameter :: BC_NONE = -1
+    integer, parameter :: BC_NEUM = 0
+    integer, parameter :: BC_DIRI = 1
+    integer, parameter :: BC_PERIODIC = 2
+    integer, parameter :: BC_CF = 3
 
 
     ! --------------------------------------------------------------------------
-    ! Used to hold data at the faces of each boundary.
+    ! Used to hold BC types and values at the domain boundary faces.
     ! --------------------------------------------------------------------------
-    type BoundaryData
+    type bdry_data
         ! Valid (not ghost) box location and size info.
-        type(Box) :: valid
+        type(box) :: valid
+
+        ! The types of BCs that the data represent.
+        integer :: type_xlo, type_xhi
+        integer :: type_ylo, type_yhi
 
         ! The boundary data that surrounds the domain.
-        real(dp), dimension(:), allocatable :: xloData
-        real(dp), dimension(:), allocatable :: xhiData
-        real(dp), dimension(:), allocatable :: yloData
-        real(dp), dimension(:), allocatable :: yhiData
-    end type BoundaryData
+        real(dp), dimension(:), allocatable :: data_xlo, data_xhi
+        real(dp), dimension(:), allocatable :: data_ylo, data_yhi
+    end type bdry_data
 
 
     ! --------------------------------------------------------------------------
     ! These objects hold the data along with its metadata.
     ! --------------------------------------------------------------------------
-    type BoxData
-        type(Box) :: bx         ! The data box. Includes ghosts.
-        type(Box) :: valid      ! The valid region only. Does not include ghosts.
+    type box_data
+        type(box) :: bx         ! The data box. Includes ghosts.
+        type(box) :: valid      ! The valid region only. Does not include ghosts.
         integer :: ngx, ngy     ! Nomber of ghosts per side in each dir
 
         ! The ghost and valid data array.
         real(dp), dimension(:,:), allocatable :: data
 
-        ! Boundary condition information
-        type(BCTypes) :: bcTypes
-        type(BoundaryData) :: bdryData
-    end type BoxData
+        ! BC info
+        type(bdry_data) :: bc
+    end type box_data
 
 
     ! --------------------------------------------------------------------------
     ! Computes inner product of data arrays for norms.
     ! --------------------------------------------------------------------------
-    interface InnerProd
-        module procedure InnerProd_Array
-        module procedure InnerProd_BoxData
+    interface inner_prod
+        module procedure inner_prod_array
+        module procedure inner_prod_box_data
     end interface
 
 contains
 
     ! --------------------------------------------------------------------------
-    ! This makes setting up a Box object a one line operation.
+    ! This makes setting up a box object a one line operation.
     ! --------------------------------------------------------------------------
-    pure subroutine DefineBox (bx, ilo, ihi, jlo, jhi, dx, dy)
-        type(Box), intent(out) :: bx
+    pure subroutine define_box (bx, ilo, ihi, jlo, jhi, dx, dy)
+        type(box), intent(out) :: bx
         integer, intent(in)    :: ilo, ihi
         integer, intent(in)    :: jlo, jhi
         real(8), intent(in)    :: dx, dy
@@ -127,24 +119,24 @@ contains
 
         bx%dx = dx
         bx%dy = dy
-    end subroutine DefineBox
+    end subroutine define_box
 
 
     ! --------------------------------------------------------------------------
-    ! This makes setting up a BoxData object a one line operation.
+    ! This makes setting up a box_data object a one line operation.
     ! --------------------------------------------------------------------------
-    subroutine DefineBoxData (bd, valid, ngx, ngy)
-        type(BoxData), intent(out) :: bd
-        type(Box), intent(in)      :: valid
-        integer, intent(in)        :: ngx, ngy
+    subroutine define_box_data (bd, valid, ngx, ngy)
+        type(box_data), intent(out) :: bd
+        type(box), intent(in)       :: valid
+        integer, intent(in)         :: ngx, ngy
 
-        type(Box) :: bx
+        type(box) :: bx
         integer   :: ierr
 
-        call DefineBox(bx, &
-                       valid%ilo-ngx, valid%ihi+ngx, &
-                       valid%jlo-ngy, valid%jhi+ngy, &
-                       valid%dx, valid%dy)
+        call define_box(bx, &
+                        valid%ilo-ngx, valid%ihi+ngx, &
+                        valid%jlo-ngy, valid%jhi+ngy, &
+                        valid%dx, valid%dy)
 
         bd%valid = valid
         bd%bx = bx
@@ -153,51 +145,51 @@ contains
 
         allocate (bd%data (bx%ilo : bx%ihi, bx%jlo : bx%jhi), stat=ierr)
         if (ierr .ne. 0) then
-            print*, 'DefineDataBox: Out of memory'
+            print*, 'define_box_data: Out of memory'
             stop
         endif
-    end subroutine DefineBoxData
+    end subroutine define_box_data
 
 
     ! --------------------------------------------------------------------------
     ! Returns true if all members of box1 and box2 are equal.
     ! --------------------------------------------------------------------------
-    pure function Compatible (bx1, bx2) result (isSame)
+    pure function compatible_boxes (bx1, bx2) result (is_same)
         implicit none
-        type(Box), intent(in) :: bx1, bx2
-        logical :: isSame
+        type(box), intent(in) :: bx1, bx2
+        logical :: is_same
 
-        isSame = .true.
+        is_same = .true.
 
-        if (bx1%ilo .ne. bx2%ilo) isSame = .false.
-        if (bx1%ihi .ne. bx2%ihi) isSame = .false.
-        if (bx1%jlo .ne. bx2%jlo) isSame = .false.
-        if (bx1%jhi .ne. bx2%jhi) isSame = .false.
+        if (bx1%ilo .ne. bx2%ilo) is_same = .false.
+        if (bx1%ihi .ne. bx2%ihi) is_same = .false.
+        if (bx1%jlo .ne. bx2%jlo) is_same = .false.
+        if (bx1%jhi .ne. bx2%jhi) is_same = .false.
 
-        if (bx1%dx .ne. bx2%dx) isSame = .false.
-        if (bx1%dy .ne. bx2%dy) isSame = .false.
-    end function Compatible
+        if (bx1%dx .ne. bx2%dx) is_same = .false.
+        if (bx1%dy .ne. bx2%dy) is_same = .false.
+    end function compatible_boxes
 
 
     ! --------------------------------------------------------------------------
     ! Sets all valid data to val.
     ! --------------------------------------------------------------------------
-    pure subroutine SetValValid (bd, val)
+    pure subroutine setval_valid (bd, val)
         implicit none
-        type(BoxData), intent(inout) :: bd
-        real(dp), intent(in)         :: val
+        type(box_data), intent(inout) :: bd
+        real(dp), intent(in)          :: val
 
         bd%data (bd%valid%ilo : bd%valid%ihi, bd%valid%jlo : bd%valid%jhi) = val
-    end subroutine SetValValid
+    end subroutine setval_valid
 
 
     ! --------------------------------------------------------------------------
     ! Sets all ghosts to val.
     ! --------------------------------------------------------------------------
-    pure subroutine SetValGhosts (bd, val)
+    pure subroutine setval_ghosts (bd, val)
         implicit none
-        type(BoxData), intent(inout) :: bd
-        real(dp), intent(in)         :: val
+        type(box_data), intent(inout) :: bd
+        real(dp), intent(in)          :: val
 
         ! The ghosts along the x-normal boundaries.
         if (bd%ngx .ge. 1) then
@@ -210,42 +202,50 @@ contains
             bd%data (bd%bx%ilo : bd%bx%ihi, bd%bx%jlo : bd%valid%jlo-1) = val
             bd%data (bd%bx%ilo : bd%bx%ihi, bd%valid%jhi+1 : bd%bx%jhi) = val
         endif
-    end subroutine SetValGhosts
+    end subroutine setval_ghosts
 
 
     ! --------------------------------------------------------------------------
     ! Computes ip = (arr1, arr2)
     ! arr1 and arr2 must be exactly the same size and rank.
     ! --------------------------------------------------------------------------
-    pure function InnerProd_Array (arr1, arr2, nx, ny) result (ip)
+    pure function inner_prod_array (arr1, arr2, nx, ny) result (ip)
         implicit none
         integer, intent(in)                      :: nx, ny
         real(dp), intent(in), dimension(1:nx*ny) :: arr1, arr2
         real(dp)                                 :: ip
 
         ip = dot_product(arr1, arr2)
-    end function InnerProd_Array
+    end function inner_prod_array
 
 
     ! --------------------------------------------------------------------------
     ! Computes ip = (bd1, bd2)
-    ! bd1 and bd2 are BoxData objects that must be exactly the same size.
+    ! bd1 and bd2 are box_data objects that must be exactly the same size.
     ! --------------------------------------------------------------------------
-    function InnerProd_BoxData (bd1, bd2) result (ip)
+    function inner_prod_box_data (bd1, bd2) result (ip)
         implicit none
-        type(BoxData), intent(in) :: bd1, bd2
-        real(dp)                  :: ip
+        type(box_data), intent(in) :: bd1, bd2
+        real(dp)                   :: ip
 
         ! Bounds checks
-        if (.not.Compatible(bd1%valid, bd2%valid)) then
-            print*, 'ERROR: InnerProd_BoxData: given BoxData are incompatible.'
+        if (.not.compatible_boxes(bd1%valid, bd2%valid)) then
+            print*, 'ERROR: inner_prod_box_data: given box_data are incompatible.'
             stop
         endif
 
         ! Compute over entire region (not just valid region).
-        ip = InnerProd_Array(bd1%data, bd2%data, bd1%bx%nx, bd1%bx%ny)
+        ip = inner_prod_array(bd1%data, bd2%data, bd1%bx%nx, bd1%bx%ny)
 
-    end function InnerProd_BoxData
+    end function inner_prod_box_data
+
+
+    ! ! --------------------------------------------------------------------------
+    ! ! --------------------------------------------------------------------------
+    ! subroutine fillGhosts (bd)
+
+    ! end subroutine fillGhosts
+
 end module ArrayUtils
 
 
